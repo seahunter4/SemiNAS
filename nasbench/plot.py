09404f1e4500618f706c9700814df19658279c49
seahunter4 <641375245@qq.com>
2020-12-30 12:29:25 +0800

    plotting

--- nasbench/plot.py
+++ nasbench/plot.py
@@ -5,15 +5,20 @@ import matplotlib.pyplot as plt
 with open("grads_data.txt", "r") as f:
     diff_str = f.readline().rstrip()
     grad_str = f.readline().rstrip()
+    label_str = f.readline().rstrip()
     diffs = diff_str.split(' ')
     grads = grad_str.split(' ')
+    labels = label_str.split(' ')
 diffs = [float(i) for i in diffs]
 grads = [float(i) for i in grads]
+labels = [float(i) for i in labels]
+
+rel_diffs = [diffs[i]/labels[i] for i in range(len(diffs))]
 
 fig = plt.figure()
 plt.xlabel('grads')
-plt.ylabel('diffs')
-plt.scatter(grads,diffs)
-plt.savefig("./grads_diffs.png")
+plt.ylabel('relative_diffs')
+plt.scatter(grads, rel_diffs)
+plt.savefig("./rel_diffs.png")
 # plt.show()
 
