5ee526f2dd1941ef7a8649710edb00996aba50fa
seahunter4 <641375245@qq.com>
2020-12-31 15:10:11 +0800

    testing file

---
+++ nasbench/test.py
@@ -0,0 +1,14 @@
+from nasbench import api
+import numpy as np
+
+all_keys = list(nasbench.hash_iterator())
+valid_accs = []
+for key in all_keys:
+    fixed_stat, computed_stat = nasbench.get_metrics_from_hash(key)
+    arch = api.ModelSpec(
+        matrix=fixed_stat['module_adjacency'],
+        ops=fixed_stat['module_operations'],
+    )
+    data = nasbench.query(arch)
+    valid_accs.append(data['validation_accuracy'])
+print("mean={}, std={}".format(np.mean(valid_accs), np.std(valid_accs)))
