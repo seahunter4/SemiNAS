4c9327ab286017fbf259ead333f4c3475d97298c
wangxuyao5 <wangxuyao5@jd.com>
2021-01-07 13:48:39 +0800

    sh file

--- nasbench/runs/train_seminas.sh
+++ nasbench/runs/train_seminas.sh
@@ -5,9 +5,9 @@ OUTPUT_DIR=outputs/$MODEL
 
 mkdir -p $OUTPUT_DIR
 
-for i in {36..50}
+for i in {1..10}
 do
   python train_seminas.py \
     --output_dir=$OUTPUT_DIR \
-    | tee $OUTPUT_DIR/log0105_$i.txt
+    | tee $OUTPUT_DIR/rlog0106_$i.txt
 done
