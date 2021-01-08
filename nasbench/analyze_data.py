2965f912a0dcf6d6a2491e6a7541396db1fd656d
wangxuyao5 <wangxuyao5@jd.com>
2021-01-08 15:21:12 +0800

    data analysis

--- nasbench/analyze_data.py
+++ nasbench/analyze_data.py
@@ -1,10 +1,14 @@
 import os
 import re
+import numpy as np
+
 
 if __name__ == "__main__":
     path = "./outputs/seminas/"
     filename_token = "log0105_"
     search_token = 'Mean test'
+    max_accs = list()
+    first_accs = list()
     for root, dirnames, filenames in os.walk(path):
         for filename in filenames:
             if re.match(filename_token, filename):
@@ -22,13 +26,17 @@ if __name__ == "__main__":
                             test_accs.append(test_acc)
                 if len(test_accs) == 20:
                     test_accs = test_accs[:10]
-                    max_acc = max(test_accs)
-                with open("test_acc.txt", "a") as f:
+                max_acc = max(test_accs)
+                max_accs.append(max_acc)
+                first_accs.append(first_acc)
+                with open("ori_test_acc.txt", "w") as f:
                     f.write("{} {}\n".format(max_acc, first_acc))
                     # for acc in test_accs:
                     #     f.write("{} ".format(acc))
                     # f.write('\n')
                 print("Successfully record test acc: {}".format(max_acc))
+    print("Mean best test acc: {}".format(np.mean(max_accs)))
+    print("Mean best val test acc: {}".format(np.mean(first_accs)))
 
 
 
